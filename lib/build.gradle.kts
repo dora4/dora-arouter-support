44f603ef9462a9e839bde90bdc27ddf0568a5bcd
dora4 <71242257+dora4@users.noreply.github.com>
2023-06-22 16:41:12 +0800

    无需再依赖arouter的库，直接依赖本库即可

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -37,9 +37,9 @@ kapt {
 dependencies {
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.github.dora4:dora:1.0.131")
     api("com.alibaba:arouter-api:1.5.2")
     kapt("com.alibaba:arouter-compiler:1.5.2")
-    api("com.github.dora4:dora:1.0.131")
 }
 
 afterEvaluate {
@@ -49,7 +49,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.dora4"
                 artifactId = "dora-arouter-support"
-                version = "1.2"
+                version = "1.3"
             }
         }
     }
