05ea0d912b7079ff228708e3be6e9995d0b38672
dora4 <71242257+dora4@users.noreply.github.com>
2023-10-12 01:14:17 +0800

    新增open扩展函数对adapter的支持

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -26,20 +26,11 @@ android {
     }
 }
 
-//这里使用Kotlin的ARouter配置，当然使用Java的也是兼容的
-kapt {
-    generateStubs = true
-    arguments {
-        arg("AROUTER_MODULE_NAME", project.name)
-    }
-}
-
 dependencies {
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.github.dora4:dora:1.0.131")
+    implementation("com.github.dora4:dora:1.1.37")
     api("com.alibaba:arouter-api:1.5.2")
-    kapt("com.alibaba:arouter-compiler:1.5.2")
 }
 
 afterEvaluate {
@@ -49,7 +40,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.dora4"
                 artifactId = "dora-arouter-support"
-                version = "1.3"
+                version = "1.4"
             }
         }
     }
