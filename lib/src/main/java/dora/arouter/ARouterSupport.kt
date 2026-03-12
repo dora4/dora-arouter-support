77d5451a2d06cf8a3a140262fcb36067d5a29cf2
Dora Chat <dora924666990@gmail.com>
2026-03-12 17:57:07 +0700

    service支持打开路由页面

--- lib/src/main/java/dora/arouter/ARouterSupport.kt
+++ lib/src/main/java/dora/arouter/ARouterSupport.kt
@@ -1,6 +1,8 @@
 package dora.arouter
 
 import android.app.Activity
+import android.app.Service
+import android.content.Intent
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.alibaba.android.arouter.facade.Postcard
@@ -17,6 +19,17 @@ fun Fragment.open(path :String, build: Postcard.()-> Unit = {}) {
     postcard.build()
     postcard.navigation()
 }
+
+fun Service.open(
+    path: String,
+    build: Postcard.() -> Unit = {}
+) {
+    val postcard = ARouter.getInstance().build(path)
+    postcard.build()
+    postcard.withFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    postcard.navigation(this)
+}
+
 
 fun RecyclerView.Adapter<*>.open(path :String, build: Postcard.()-> Unit = {}) {
     val postcard = ARouter.getInstance().build(path)
